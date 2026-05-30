b15dae24792ad03cfcb58d4a9a23b4e2ea7d26a5
kwonsungja <4gg4g4h4@gmail.com>
2026-05-30 21:52:14 +0900

    Update 4_APP_Collocations.py

--- pages/4_APP_Collocations.py
+++ pages/4_APP_Collocations.py
@@ -294,7 +294,7 @@ for i, item in enumerate(st.session_state.meaning_quiz_items, start=1):
     key=f"m_{i}"
 )
 
-        answers.append((item, answer, correct))
+    answers.append((item, answer, correct))
 
     if st.button("제출하기"):
         score = 0
